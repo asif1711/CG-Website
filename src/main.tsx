c79d6be9037dde2833b4022ef3aa0b43c7f40b01
Nurul Islam <asif17111998@gmail.com>
2026-06-26 18:33:36 +0530

    fix(dom): prevent DOM mutation errors in React
    Simplify Node prototype patches to avoid recursive or invalid DOM operations when nodes are detached from the parent, preventing potential application crashes caused by third-party script conflicts.

--- src/main.tsx
+++ src/main.tsx
@@ -9,9 +9,6 @@ if (typeof Node !== 'undefined' && Node.prototype) {
   const originalRemoveChild = Node.prototype.removeChild;
   Node.prototype.removeChild = function <T extends Node>(child: T): T {
     if (child.parentNode !== this) {
-      if (child.parentNode) {
-        return originalRemoveChild.call(child.parentNode, child) as T;
-      }
       return child;
     }
     return originalRemoveChild.call(this, child) as T;
@@ -20,10 +17,7 @@ if (typeof Node !== 'undefined' && Node.prototype) {
   const originalInsertBefore = Node.prototype.insertBefore;
   Node.prototype.insertBefore = function <T extends Node>(newNode: T, referenceNode: Node | null): T {
     if (referenceNode && referenceNode.parentNode !== this) {
-      if (referenceNode.parentNode) {
-        return originalInsertBefore.call(referenceNode.parentNode, newNode, referenceNode) as T;
-      }
-      return originalInsertBefore.call(this, newNode, null) as T;
+      return newNode;
     }
     return originalInsertBefore.call(this, newNode, referenceNode) as T;
   };
@@ -31,9 +25,6 @@ if (typeof Node !== 'undefined' && Node.prototype) {
   const originalReplaceChild = Node.prototype.replaceChild;
   Node.prototype.replaceChild = function <T extends Node>(newChild: Node, oldChild: T): T {
     if (oldChild.parentNode !== this) {
-      if (oldChild.parentNode) {
-        return originalReplaceChild.call(oldChild.parentNode, newChild, oldChild) as T;
-      }
       return oldChild;
     }
     return originalReplaceChild.call(this, newChild, oldChild) as T;
